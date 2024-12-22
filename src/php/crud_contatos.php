28bb4367bdfb35c62027cbb7804d4303b1be71cf
Gabriel Teixeira Charles da Silva <charlesgabriel102@gmail.com>
2024-12-22 18:24:46 -0300

    Nova ação disponível: "create"

--- src/php/crud_contatos.php
+++ src/php/crud_contatos.php
@@ -1,3 +1,35 @@
 <?php
 
 include "conexao.php";
+
+// verifica se a ação foi passada
+if(!isset($_REQUEST['action'])){
+    die("Nenhuma ação específicada");
+}
+
+$action = $_REQUEST['action'];
+
+
+switch($action){
+
+    case 'create':
+
+        $nome = $_POST['name'];
+        $email = $_POST['email'];
+        $telefone = $_POST['phone'];
+        $endereco = $_POST['address'];
+        $data_aniversario = $_POST['birthday'];
+        $notas = $_POST['notes'];
+        $isFavorito = isset($_POST['favorite']) ? 1 : 0;
+
+        $sql = $conn->prepare('INSERT INTO contatos (nome, email, telefone, endereco, data_aniversario, notas, isFavorito) VALUES (?, ?, ?, ?, ?, ?, ?)');
+        $sql->bind_param('sssssi', $nome, $email, $telefone, $endereco, $data_aniversario, $notas, $isFavorito);
+        
+        $sql->close();
+        
+        break;
+    default: 
+        echo "Ação inválida";
+        break;
+
+}
