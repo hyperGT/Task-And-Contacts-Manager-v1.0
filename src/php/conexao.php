e71b46b75e1d8070641c06cb6e181e05923eda94
Gabriel Teixeira Charles da Silva <charlesgabriel102@gmail.com>
2024-12-22 18:24:17 -0300

    conexão com o banco de dados feita

--- src/php/conexao.php
+++ src/php/conexao.php
@@ -1,2 +1,12 @@
 <?php
 
+$server = "localhost";
+$username = "root";
+$pass = "";
+$db_name = "tasksContactsMngr";
+
+$conn = new mysqli($server, $username, $pass, $db_name);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
